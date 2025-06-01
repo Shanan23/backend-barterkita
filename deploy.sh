ee6454e9043b209b98b56c14bdd8634af58906bd
Gandha Ryanto <ganda@mdd.co.id>
2025-06-02 01:31:25 +0700

    Update deployment script to accept version parameter for Docker image tagging

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 # This script automates the deployment of a Dockerized application.
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
 echo "Starting deployment process..."
 git pull
 
-# Build the Docker image with no cache
-echo "Building Docker image with no cache..."
-docker build --no-cache -t backend-barterkita:latest .
+# Build the Docker image with no cache and version tag
+echo "Building Docker image with no cache and version tag $VERSION..."
+docker build --no-cache -t backend-barterkita:$VERSION .
 
 # Stop and remove existing containers
 echo "Stopping and removing existing containers..."
